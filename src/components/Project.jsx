1bbeb0ebc8ffbe40a71663617812b541ed6baa50
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-09 23:55:58 +0100

    feat: project component

---
+++ src/components/Project.jsx
@@ -0,0 +1,12 @@
+import '../assets/scss/components/Project.scss';
+import '../assets/scss/index.scss'
+
+function Project() {
+    return(
+        <div id="project">
+            <p>Project</p>
+        </div>
+    )
+}
+
+export default Project
