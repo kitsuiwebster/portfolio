759248c230090ca53a4cd3280aa75da2adcd6c1e
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-09 01:46:26 +0100

    fix: routes

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import Main from './pages/Main';
+import Articles from './pages/Articles';
+import HackAPrompt from './pages/articles/HackAPrompt';
+import Layout from './layouts/Layout';
 import NotFound from './pages/NotFound';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter } from "react-router-dom";
 
-const AppRouter = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </Router>
-);
+const AppRouter = createBrowserRouter([
+    {
+      path: "/",
+      element:<Layout><Main /></Layout>
+    },
+    {
+      path: "/articles",
+      element:<Layout><Articles /></Layout>
+    },
+    {
+      path: "/articles/hackaprompt2023",
+      element:<Layout><HackAPrompt /></Layout>
+    },
+    {
+      path: "*",
+      element:<NotFound />
+    }
+  ]);
+  
 
 export default AppRouter;
