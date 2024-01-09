274f00c9b0adf9ffe05c2c9aa8b01de0dfd7c7df
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-09 23:56:49 +0100

    feat: skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,12 @@
+import '../assets/scss/components/Skills.scss';
+import '../assets/scss/index.scss'
+
+function Skills() {
+    return(
+        <div id="skills">
+            <p>Skills</p>
+        </div>
+    )
+}
+
+export default Skills
