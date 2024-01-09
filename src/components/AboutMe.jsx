ba667f069b1d1bc3a61ac8892791f63b0eed33c9
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-09 23:53:28 +0100

    feat: about me component

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,12 @@
+import '../assets/scss/components/AboutMe.scss';
+import '../assets/scss/index.scss'
+
+function AboutMe() {
+    return(
+        <div id="aboutme">
+            <p>AboutMe</p>
+        </div>
+    )
+}
+
+export default AboutMe
