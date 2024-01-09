a1d3ba6ad623fb1569198619a9aee09a5a2261ef
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-09 23:56:22 +0100

    feat: contact component

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import '../assets/scss/pages/Contact.scss';
+import '../assets/scss/index.scss'
+
+function Contact() {
+    return(
+        <div id="contact">
+            <p>Contact</p>
+        </div>
+    )
+}
+
+export default Contact
