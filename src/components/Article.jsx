8f613229e83627c2ca9ddab2ef286e0b3dc07eb9
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-25 01:49:58 +0100

    wip: articles page

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,12 +1,20 @@
 import '../assets/scss/components/Article.scss';
-import '../assets/scss/index.scss'
+import '../assets/scss/index.scss';
 
-function Article() {
-    return(
+function Article({ title, description, firstIconSrc, firstIconAlt, secondIconSrc, secondIconAlt, thirdIconSrc, thirdIconAlt }) {
+    return (
         <div id="article">
-            <p>article</p>
+            <div className="article">
+                <h1 className='article-title'>{title}</h1>
+                <p className='article-description'>{description}</p>
+                <div className='article-icons'>
+                    <img src={firstIconSrc} alt={firstIconAlt} className="article-icons-img" />
+                    <img src={secondIconSrc} alt={secondIconAlt} className="article-icons-img" />
+                    <img src={thirdIconSrc} alt={thirdIconAlt} className="article-icons-img" />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Article
+export default Article;
--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom';
 import '../assets/scss/pages/Articles.scss';
-import '../assets/scss/index.scss'
+import '../assets/scss/index.scss';
+import Article from '../components/Article';
+
 
 function Articles() {
     return (
         <>
             <div id="articles">
-                <p>Articles</p>
-                <div>
-                    <Link to="/articles/hackaprompt2023">Hack A Prompt</Link>
-                    <Link to="/articles/minecraft-java-admin-commands">Minecraft Server Java Administrator Commands</Link>
+                <div className="articles">
+                    <p className='articles-title'>Articles</p>
+                    <Link to="/articles/hackaprompt2023" className="articles-link">
+                        <Article title="Hack A Prompt" description="oui" /> 
+                    </Link>
+                    <Link to="/articles/minecraft-java-admin-commands" className="articles-link">
+                        <Article title="Minecraft Server Java Administrator Commands" description="non" /> 
+                    </Link>
                 </div>
             </div>
         </>
