4880a908e9cf20af2326daa744ec7cd2d3ccb7d7
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-27 02:21:42 +0100

    feat: the legend of dihia

---
+++ src/pages/Dihia.jsx
@@ -0,0 +1,63 @@
+import styles from '../assets/css/Dihia.module.css';
+
+function Dihia() {
+    return (
+        <>
+            <div className={styles.dihiawrap}>
+                <div className={styles.moon}></div>
+
+                <div className={styles.container}>
+                    <h1 className={styles.title}>La légende de Dihia</h1>
+                    <ul className={styles.ul}>
+                        <li className={styles.slideanimation}>📚 La légende raconte que quand Dihia était petite, c'est elle qui racontait des histoires à ses parents.</li>
+                        <li className={styles.slideanimation}>🏠 La légende raconte que le jour où Dihia a quitté son domicile familial, elle a dit à son père maintenant c'est toi le chef de la maison.</li>
+                        <li className={styles.slideanimation}>🎂 La légende raconte que lorsque Dihia a eu 18 ans, ce sont ses parents qui ont quitté le domicile familial.</li>
+                        <li className={styles.slideanimation}>☀️ La légende raconte qu'il y a quelques années Dihia a séché les cours pendant 3 mois et depuis on appelle cela les vacances d'été.</li>
+                        <li className={styles.slideanimation}>📞 La légende raconte que quand Bell a inventé le téléphone, il a trouvé 2 appels manqués de Dihia.</li>
+                        <li className={styles.slideanimation}>⏰ La légende raconte que tous les matins c'est Dihia qui réveille son alarme.</li>
+                        <li className={styles.slideanimation}>🎖️ La légende raconte que Dihia n'a pas besoin de se faire recommander, c'est elle qui recommande.</li>
+                        <li className={styles.slideanimation}>🍪 La légende raconte que quand Dihia va utiliser Google Chrome, c'est elle qui distribue les cookies.</li>
+                        <li className={styles.slideanimation}>💻 La légende raconte que quand Dihia installe Windows, c'est Microsoft qui accepte ses conditions d'utilisation.</li>
+                        <li className={styles.slideanimation}>☀️ La légende raconte que Dihia n'a pas peur des rayons du soleil, c'est le soleil qui a peur de Dihia.</li>
+                        <li className={styles.slideanimation}>🌞 La légende raconte que quand Dihia prend des coups de soleil, c'est le soleil qui a mal.</li>
+                        <li className={styles.slideanimation}>🍴 La légende raconte que quand Dihia va au restaurant, c'est elle qui demande le pourboir.</li>
+                        <li className={styles.slideanimation}>🏡 La légende raconte que quand Dihia vient chez toi, c'est toi l'invité.</li>
+                        <li className={styles.slideanimation}>🚌 La légende raconte que quand Dihia monte dans le bus, tout le monde descend.</li>
+                        <li className={styles.slideanimation}>🎫 La légende raconte que quand les contrôleurs montent dans le bus, c'est elle qui vérifie leur titre de transport.</li>
+                        <li className={styles.slideanimation}>🚗 La légende raconte que quand Dihia roule en voiture, c'est les limites de vitesses qui la respectent.</li>
+                        <li className={styles.slideanimation}>👮 La légende raconte que Dihia protège ses agents de sécurité.</li>
+                        <li className={styles.slideanimation}>⚖️ La légende raconte que quand Dihia va dans un tribunal, c'est le juge qui l'appelle 'Votre honneur'.</li>
+                        <li className={styles.slideanimation}>🔢 La légende raconte que Dihia a déjà compté jusqu'à l'infini 3 fois d'affilé.</li>
+                        <li className={styles.slideanimation}>💍 La légende raconte que quand Dihia est invitée à un mariage, c'est les mari</li>
+                        <li className={styles.slideanimation}>🚶 La légende raconte que ce n'est pas Dihia qui marche, c'est la Terre qui roule sous ses pieds.</li>
+                        <li className={styles.slideanimation}>🌅 La légende raconte que quand Dihia se lève et ouvre la fenêtre, c'est le soleil qui est ébloui.</li>
+                        <li className={styles.slideanimation}>🌠 La légende raconte que quand Dihia regarde les étoiles, ce sont les étoiles qui font un voeu.</li>
+                        <li className={styles.slideanimation}>🎙️ La légende raconte que quand Dihia chante, c'est les notes qui cherchent à s'accorder à sa voix.</li>
+                        <li className={styles.slideanimation}>🔦 La légende raconte que quand Dihia allume une lampe, c'est l'obscurité qui se cache.</li>
+                        <li className={styles.slideanimation}>🌍 La légende raconte que quand Dihia est née, c'est le monde qui était ébloui.</li>
+                        <li className={styles.slideanimation}>👋 La légende raconte que durant son enfance, lorsque Dihia faisait une bêtise, c'est elle qui donnait la fessée</li>
+                    
+
+                        <li className={styles.slideanimation}>📖 La légende ne raconte rien, c'est Dihia qui raconte la légende.</li>
+                    </ul>
+                </div>
+
+
+                <div className={styles.backgroundwrap}>
+                    <div className={`${styles.bubble} ${styles.x1}`}></div>
+                    <div className={`${styles.bubble} ${styles.x2}`}></div>
+                    <div className={`${styles.bubble} ${styles.x3}`}></div>
+                    <div className={`${styles.bubble} ${styles.x4}`}></div>
+                    <div className={`${styles.bubble} ${styles.x5}`}></div>
+                    <div className={`${styles.bubble} ${styles.x6}`}></div>
+                    <div className={`${styles.bubble} ${styles.x7}`}></div>
+                    <div className={`${styles.bubble} ${styles.x8}`}></div>
+                    <div className={`${styles.bubble} ${styles.x9}`}></div>
+                    <div className={`${styles.bubble} ${styles.x10}`}></div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Dihia;
