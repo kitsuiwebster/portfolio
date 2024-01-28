158fa7f0314ce7e31bd2d2c575ded253041ef35f
kitsuiwebster <raphmartin1999@gmail.com>
2024-01-28 13:23:51 +0100

    feat: projects kasa and cozybot

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,16 +1,25 @@
 import '../assets/scss/components/Project.scss';
 import '../assets/scss/index.scss'
+import ProjectIcon from './ProjectIcon';
 
-function Project({ title, description, firstIconSrc, firstIconAlt, secondIconSrc, secondIconAlt, thirdIconSrc, thirdIconAlt }) {
+function Project({ title, description, imageSrc, imageAlt, logoSrc0, logoSrc1, logoSrc2, logoSrc3, logoSrc4, logoSrc5 }) {
+
+    
     return(
         <div id="project">
             <div className="project">
                 <h1 className='project-title'>{title}</h1>
                 <p className='project-description'>{description}</p>
+                <div className='project-image'>
+                    <img className='project-image-img' src={imageSrc} alt={imageAlt}></img>
+                </div>
                 <div className='project-icons'>
-                    <img src={firstIconSrc} alt={firstIconAlt} className="project-icons-img" />
-                    <img src={secondIconSrc} alt={secondIconAlt} className="project-icons-img" />
-                    <img src={thirdIconSrc} alt={thirdIconAlt} className="project-icons-img" />
+                    <ProjectIcon src={logoSrc0}/>
+                    <ProjectIcon src={logoSrc1}/>
+                    <ProjectIcon src={logoSrc2}/>
+                    <ProjectIcon src={logoSrc3}/>
+                    <ProjectIcon src={logoSrc4}/>
+                    <ProjectIcon src={logoSrc5}/>
                 </div>
             </div>
         </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,17 +3,58 @@ import '../assets/scss/pages/Projects.scss';
 import '../assets/scss/index.scss'
 import Project from '../components/Project';
 
+// Technos icons
+import pythonLogo from '../assets/images/technos/python.png';
+import gitLogo from '../assets/images/technos/git.png';
+import zshLogo from '../assets/images/technos/zsh.png';
+import discordLogo from '../assets/images/social/discord.png';
+import raspberryLogo from '../assets/images/technos/raspberry-pi.png';
+import condaLogo from '../assets/images/technos/conda.png';
+import reactLogo from '../assets/images/technos/react.png';
+import htmlLogo from '../assets/images/technos/html.png';
+import sassLogo from '../assets/images/technos/sass.png';
+import jsLogo from '../assets/images/technos/js.png';
+
+// Projects images
+import kasaImage from '../assets/images/projects/kasa.png'
+import cozybotImage from '../assets/images/projects/cozybot.png'
+
+
 function Projects() {
     return (
         <>
             <div id="projects">
                 <div className="projects">
-                    <p className='projects-title'>Projects</p>
+                    <h1 className='projects-title'>Projects</h1>
+                    <h2 className='projects-subtitle'>School Projects</h2>
                     <Link to="/projects/cozybot" className="projects-link">
                         <Project
-                        title="CozyBot: A Discord.py bot"
-                        description="Bring tranquility to your Discord server with CozyBot. This innovative bot streams peaceful nature ambiences into voice channels on-demand."/> 
-                        
+                        title="Kasa: A real estate rental agency"
+                        description="Bring tranquility to your Discord server with CozyBot. This innovative bot streams peaceful nature ambiences into voice channels on-demand." 
+                        imageSrc={kasaImage}
+                        imageAlt="Kasa"
+                        logoSrc0={reactLogo}
+                        logoSrc1={htmlLogo}
+                        logoSrc2={sassLogo}
+                        logoSrc3={jsLogo}
+                        alt="Python"
+                        />
+                    </Link>
+                    <h2 className='projects-subtitle'>Other Projects</h2>
+                    <Link to="/projects/cozybot" className="projects-link">
+                        <Project
+                        title="CozyBot: A Discord bot that brings peace in voice channels"
+                        description="Bring tranquility to your Discord server with CozyBot. This innovative bot streams peaceful nature ambiences into voice channels on-demand." 
+                        imageSrc={cozybotImage}
+                        imageAlt="CozyBot"
+                        logoSrc0={pythonLogo}
+                        logoSrc1={discordLogo}
+                        logoSrc2={condaLogo}
+                        logoSrc3={raspberryLogo}
+                        logoSrc4={zshLogo}
+                        logoSrc5={gitLogo}
+                        alt="Python"
+                        />
                     </Link>
                 </div>
             </div>
