fb7ece01bee5462f2185b5382353e34ad8771349
kitsuiwebster <raphmartin1999@gmail.com>
2024-02-01 03:32:51 +0100

    feat: implement i18next

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppRouter from './AppRouter';
 import { RouterProvider } from 'react-router-dom';
-import { ThemeProvider } from './ThemeContext'; 
+import { ThemeProvider } from './ThemeContext';
+import i18n from './i18n'; 
+import { I18nextProvider } from 'react-i18next'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider> 
-      <RouterProvider router={AppRouter}/>
+    <ThemeProvider>
+      <I18nextProvider i18n={i18n}>
+        <RouterProvider router={AppRouter}/>
+      </I18nextProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
