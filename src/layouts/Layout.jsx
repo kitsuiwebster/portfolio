be1aa392b9d2c566bb347e3156ca1343f227295c
kitsuiwebster <raphmartin1999@gmail.com>
2024-02-01 03:34:38 +0100

    feat: languages toggle button & menu

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -4,6 +4,10 @@ import React, { useState } from 'react';
 import menuIcon from '../assets/images/menu.png'
 import { useTheme } from '../ThemeContext';
 
+import { useTranslation } from 'react-i18next';
+import ukFlag from '../assets/images/flags/uk.png'
+import frenchFlag from '../assets/images/flags/fr.png'
+
 // Social links
 import gitlabLogo from '../assets/images/social/gitlab.png';
 import githubLogo from '../assets/images/social/github.png';
@@ -26,9 +30,28 @@ function copyDiscordUsername() {
 }
 
 function Layout({ children }) {
-    const [isNavOpen, setIsNavOpen] = useState(false);
 
+    const { t } = useTranslation('layout');
+    const [isNavOpen, setIsNavOpen] = useState(false);
     const { theme, toggleTheme } = useTheme();
+
+
+
+    const [isLangMenuOpen, setIsLangMenuOpen] = useState(false);
+    const { i18n } = useTranslation();
+
+    const currentLanguage = i18n.language;
+    const otherLanguage = currentLanguage === 'en' ? 'fr' : 'en';
+    const currentFlag = currentLanguage === 'en' ? ukFlag : frenchFlag;
+
+    const toggleLangMenu = () => {
+        setIsLangMenuOpen(!isLangMenuOpen);
+    };
+
+    const changeLanguage = (lang) => {
+        i18n.changeLanguage(lang);
+        setIsLangMenuOpen(false);
+    };
 
     const toggleNav = () => {
         setIsNavOpen(!isNavOpen);
@@ -45,14 +68,31 @@ function Layout({ children }) {
                     </div>
                     <nav className={`layout-header-nav ${isNavOpen ? 'open' : ''}`}>
                         <ul className='layout-header-nav-ul'>
-                            <li className="layout-header-nav-ul-li"> <a className="layout-header-nav-ul-li-a" href="/" >Home</a></li>
-                            <li className="layout-header-nav-ul-li"> <a className="layout-header-nav-ul-li-a" href="/projects">Projects</a></li>
-                            <li className="layout-header-nav-ul-li"> <a className="layout-header-nav-ul-li-a" href="/articles">Articles</a></li>
-                            <li className="layout-header-nav-ul-li"> <a className="layout-header-nav-ul-li-a" href="/contact">Contact</a></li>
+                            <li className="layout-header-nav-ul-li"> <a className="layout-header-nav-ul-li-a" href="/" >{t('header.home')}</a></li>
+                            <li className="layout-header-nav-ul-li"> <a className="layout-header-nav-ul-li-a" href="/projects">{t('header.projects')}</a></li>
+                            <li className="layout-header-nav-ul-li"> <a className="layout-header-nav-ul-li-a" href="/articles">{t('header.articles')}</a></li>
+                            <li className="layout-header-nav-ul-li"> <a className="layout-header-nav-ul-li-a" href="/contact">{t('header.contact')}</a></li>
                         </ul>
                     </nav>
-                    <button onClick={toggleTheme} className={`layout-header-toggle-button ${theme}`}>
-                    </button>
+                    <div className="layout-header-toggle">
+                        <div className="layout-header-toggle-language">
+                            <img src={currentFlag} alt="Language" onClick={toggleLangMenu} className="layout-header-toggle-language-icon" />
+                            {isLangMenuOpen && (
+                                <div className="layout-header-toggle-language-menu">
+                                    {otherLanguage === 'en' && (
+                                        <img className='layout-header-toggle-language-menu-icon'
+                                        src={ukFlag} alt="United Kingdom flag" onClick={() => changeLanguage('en')}/>
+                                    )}
+                                    {otherLanguage === 'fr' && (
+                                        <img className='layout-header-toggle-language-menu-icon'
+                                        src={frenchFlag} alt="France flag" onClick={() => changeLanguage('fr')}/>
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                        <button onClick={toggleTheme} className={`layout-header-toggle-theme ${theme}`}>
+                        </button>
+                    </div>
                 </header>
 
                 <main className='layout-main'>
@@ -83,7 +123,7 @@ function Layout({ children }) {
                             <img className="layout-footer-link-img" src={discordLogo} alt="Discord"/>
                         </div>
                     </nav>
-                    <p className='layout-footer-text'>© Copyright @kitsuiwebster All Rights Reserved</p>
+                    <p className='layout-footer-text'>{t('footer.copyright')}</p>
                 </footer>
             </div>
         </>
